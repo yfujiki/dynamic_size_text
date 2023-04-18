34021edcf4d8fc41767d139d21e8301efe903909
Yuichi Fujiki <fjk89025@gmail.com>
2023-04-18 15:15:00 +0900

    Implemented the tabs

---
+++ lib/standard_widgets_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Home tab'),
+    );
+  }
+}
